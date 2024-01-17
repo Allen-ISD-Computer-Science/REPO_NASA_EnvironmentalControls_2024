2a500e2acc8140701df1e58156b4525a1afd1cf7
shreyas-newa <newashreyas@gmail.com>
2024-01-16 20:27:50 -0600

    Added basic shape of UI and some text

--- NasaEC/App.js
+++ NasaEC/App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nasa HUNCH</Text>
+      
+      <View style={styles.rectangle}>
+      <Text style={styles.rectangleText}>Temperature Sensor</Text>
+      <View style={styles.rectangle2}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rectangle: {
+    width: 770,
+    height: 1080,
+    backgroundColor: 'white', 
+    marginTop: -10, 
+    marginLeft: 20, 
+    marginRight:'auto',
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  rectangleText: {
+    color: 'black',
+    fontSize: 40, // Adjust the font size
+    fontWeight: 'bold',
+  },
+  rectangle2: {
+    position: 'absolute',
+    top: 60,
+    left: 40,
+    width: 200,
+    height: 100,
+    backgroundColor: 'red',
+    borderWidth: 2,
+    borderColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });

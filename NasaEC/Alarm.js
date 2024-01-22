17def1cddd8f885b44e14ad890b5a000a965b356
rishibadal726 <rishibadal873@gmail.com>
2024-01-22 12:39:15 -0600

    New Alarm page #26

---
+++ NasaEC/Alarm.js
@@ -0,0 +1,5 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
+import { Dimensions } from 'react-native';
+import React, { Component } from 'react';
+
